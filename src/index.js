b87b771eea0de6e9407c130ff921256a2e02982b
Oscar Sanchez <oscarsanchez@Oscars-MacBook-Pro.local>
2020-08-25 14:47:24 -0500

    I need more milk, Arnold -West World

--- src/index.js
+++ src/index.js
@@ -2,40 +2,46 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 
+const Workers = () => {};
+
 const DepartmentList = (props) => {
-    console.log('props ', props)
-    return props.department
+  //console.log('props ', props)
+  console.log('departments ', props.department);
+  const departmentArr = props.department.map((depo) => (
+    <div>
+      <h4>{depo.name}</h4>
+      <ul data-id={depo.id} key={depo.id}></ul>
+    </div>
+  ));
+  return departmentArr;
+};
+
+class Acme extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      departments: [],
+    };
+  }
+  async componentDidMount() {
+    try {
+      const res = await axios.get('api/departments');
+      const departments = res.data;
+      //console.log('in DidMount ', departments)
+      await this.setState({ departments: departments });
+    } catch (err) {
+      console.log('not mounted');
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <DepartmentList department={this.state.departments} />
+      </div>
+    );
+  }
 }
 
-class Acme extends React.Component{
-    constructor() {
-        super()
-        this.state = {
-            departments: []
-        }
-        
-    }
-    async componentDidMount() {
-        try {
-            const res = await axios.get('api/departments');
-            const departments = res.data;
-            console.log('in DidMount ', departments)
-            await this.setState({departments: departments});
-        } catch(err) {
-            console.log('not mounted')
-        }
-    }
-
-    render() {
-        return (
-            <div>
-                <ul>
-                    <DepartmentList department={this.state.departments} />
-                </ul>
-            </div>
-        )
-    }
-
-}
-
-ReactDOM.render(<Acme />, document.getElementById('main'))
+ReactDOM.render(<Acme />, document.getElementById('root'));
+//<DepartmentList department={this.state.departments} />
